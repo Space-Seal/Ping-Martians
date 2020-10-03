0ae4fd07deeef95a432b5437422a38bed75770dc
Jasper Yu <007seadog@gmail.com>
2020-10-04 03:31:11 +0800

    Fullscreen click in login screen

--- src/scene/welcome.js
+++ src/scene/welcome.js
@@ -1,4 +1,3 @@
-var hint_tween;
 var SceneWelcome = new Phaser.Class({
 
 	Extends: Phaser.Scene,
@@ -13,7 +12,6 @@ var SceneWelcome = new Phaser.Class({
 	preload: function ()
 	{
 		this.load.image('logo', 'assets/logo.png');
-		this.load.image('hint', 'assets/button.png');
 		this.load.image('bg', 'assets/bg_welcome.png');
 	},
 
@@ -21,15 +19,13 @@ var SceneWelcome = new Phaser.Class({
 	{
 		var bg = this.add.image(0, 0, 'bg').setOrigin(0).setScale(0.711);
 		var logo = this.add.image(700, 350, 'logo').setScale(0.7);
-		var hint = this.add.image(900, 600, 'hint');
-		var btn = this.add.rectangle(680, 620, 180, 200, 0xff0000, 0.0);
-		hint.setAlpha(0)
-		this.tweens.add({'targets': bg, 'y': -690, 'duration': 5000}).on('complete', () => {btn.setInteractive({ cursor: 'pointer' });hint_tween = this.tweens.add({'targets': hint, 'alpha': 1, 'duration': 15000});});
+		var btn = this.add.rectangle(683, 384, 1366, 768, 0xff0000, 0.0);
+		this.tweens.add({'targets': bg, 'y': -690, 'duration': 5000}).on('complete', () => {btn.setInteractive({ cursor: 'pointer' });});
 		this.tweens.add({'targets': logo, 'y': 250, 'duration': 4000});
 		btn.once('pointerdown', function (event) {
 			//this.scene.transition({ target: 'sceneStoryA1', duration: 0});
 			this.tweens.add({targets: bg, 'scale': 1.3, 'x': -560, 'y': -1900, 'alpha': 0,'duration': 6000}).on('complete', () => {this.scene.transition({ target: 'sceneWelcomeVideo', duration: 0})})
-			this.tweens.add({'targets': [logo, hint], 'alpha': 0, 'duration': 1000}).on('start', () => {hint_tween.pause()});
+			this.tweens.add({'targets': [logo], 'alpha': 0, 'duration': 1000}).on();
 		}, this);
 	}
 
