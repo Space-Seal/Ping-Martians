19859fddab2ef835107d008a54387a603a69509c
Andrew <andrewwang101@gmail.com>
2020-10-04 12:32:58 +0800

    Add Warning Icon

--- src/scene/storyA_1.js
+++ src/scene/storyA_1.js
@@ -1,3 +1,10 @@
+var StoryA1DATA={
+	warningimg: '',
+	level: 0,
+	fadecount: 0,
+	fadetime: 30,
+	fadedir:1,
+};
 var SceneStoryA1 = new Phaser.Class({
 
 	Extends: Phaser.Scene,
@@ -6,7 +13,7 @@ var SceneStoryA1 = new Phaser.Class({
 
 	function SceneStoryA1 ()
 	{
-		Phaser.Scene.call(this, { key: 'sceneStoryA1' });
+		Phaser.Scene.call(this, { key: 'sceneStoryA1' ,	StoryA1DATA});
 	},
 
 	preload: function ()
@@ -20,6 +27,7 @@ var SceneStoryA1 = new Phaser.Class({
 		this.load.image('ad1', 'assets/screen_logo_1.png');
 		this.load.image('ad2', 'assets/screen_logo_2.png');
 		this.load.image('fg_StoryA1', 'assets/fg_storyA1_effect.png');
+		this.load.image('warning',	'assets/warning.png')
 	},
 
 	create: function ()
@@ -30,6 +38,7 @@ var SceneStoryA1 = new Phaser.Class({
 		var obapp = this.add.image(800, 410, 'ob_app').setScale(0.7);
 		var machine2 = this.add.image(350, 670, 'laptop').setScale(0.7);
 		var machine3 = this.add.image(1230, 640, 'cactus').setScale(0.7);
+		StoryA1DATA.warningimg = this.add.image(470, 350, 'warning').setOrigin(0).setScale(0.02);
 		var ads = [];
 		ads.push(this.add.image(180, 330, 'ad1').setScale(0.7));
 		ads.push(this.add.image(180, 330, 'ad2').setScale(0.7));
@@ -61,5 +70,33 @@ var SceneStoryA1 = new Phaser.Class({
 		machine3.setInteractive({ cursor: 'pointer' });
 		obapp.setInteractive({ cursor: 'pointer' });
 		ads[0].setInteractive({ cursor: 'pointer' });
+	},
+
+	update: function() {
+		if(currentGameProgress <30)
+		{
+			if(StoryA1DATA.fadecount >= StoryA1DATA.fadetime)
+			{
+				StoryA1DATA.fadedir*=(-1);
+				StoryA1DATA.fadecount = 0;
+			}
+			if(StoryA1DATA.fadedir == 1)
+			{
+				StoryA1DATA.warningimg.alpha = StoryA1DATA.fadecount/StoryA1DATA.fadetime;
+			}
+			else
+			{
+				StoryA1DATA.warningimg.alpha = 1-StoryA1DATA.fadecount/StoryA1DATA.fadetime;
+			}
+			StoryA1DATA.fadecount++;
+		}
+		if(currentGameProgress >=15)
+		{
+			StoryA1DATA.warningimg.x = 655;
+		}
+		if(currentGameProgress >= 30)
+		{
+			StoryA1DATA.warningimg.alpha = 0;
+		}
 	}
 });
