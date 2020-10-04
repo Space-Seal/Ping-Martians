87992cc75d6fd3ad576d6459a2abb55c9c314d92
Andrew <andrewwang101@gmail.com>
2020-10-04 10:54:38 +0800

    SceneEmail bug fix

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ var config = {
 			gravity: { y: 200 }
 		}
 	},
-	scene: [SceneWelcome, SceneBinaryGame, SceneWelcomeVideo, SceneGameC, SceneStoryA1, SceneMachine1, SceneMachine2, SceneMachine3, SceneStarmap],
+	scene: [SceneWelcome, SceneBinaryGame, SceneWelcomeVideo, SceneGameC, SceneStoryA1, SceneMachine1, SceneMachine2, SceneMachine3, SceneStarmap, SceneEmail,
 	//scene: [SceneBinaryGame, SceneWelcome, SceneWelcomeVideo, SceneGameC, SceneStoryA1, SceneMachine1, SceneMachine2, SceneMachine3, SceneStarmap],
 	dom: {createContainer: true}
 };
